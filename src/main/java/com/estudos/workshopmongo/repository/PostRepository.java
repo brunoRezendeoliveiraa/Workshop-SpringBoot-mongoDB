9a815fa10f1ebf5ad2f6561538710573d27a4c67
brunoRezendeoliveiraa <brunooliveiratecno@gmail.com>
2024-11-15 18:07:18 +0100

    Consulta simples com @Query

--- src/main/java/com/estudos/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/estudos/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.estudos.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.estudos.workshopmongo.domain.Post;
@@ -11,5 +12,9 @@ import com.estudos.workshopmongo.domain.Post;
 //Com isso seremos capazes de realizar o CRUD no mongorepository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+	//Função não utilizada por conta do @Query abaixo.
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
 }
